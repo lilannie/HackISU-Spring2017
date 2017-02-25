fdf60f24686a4ba13ec36cbd6e2aa0699f3ad3d8
Annie Steenson <anniesteenso@gmail.com>
2017-02-24 23:01:41 -0600

    Set up dev server for react/front-end development

--- client/app/router.js
+++ client/app/router.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Route, Router, IndexRoute, browserHistory} from 'react-router';
+import {Route, Router, browserHistory} from 'react-router';
+
+import Main from 'Main';
 
 ReactDOM.render(
     <Router history={browserHistory}>
-        <Route path="/" component={Main}>
-            <IndexRoute component={Index}/>
-        </Route>
+        <Route path="/" component={Main}/>
     </Router>,
     document.getElementById('app')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,24 @@
-var path = require('path'),
-    paths = {
-        entry: path.join(__dirname, '..', 'client', 'app', 'router.js'),
-        build: path.join(__dirname, '..', 'public'),
-        output: path.join(__dirname, '..', 'public', 'bundle.js')
-    };
+var webpack = require('webpack');
+var path = require('path');
+var paths = {
+    entry: path.join(__dirname, 'client', 'app', 'router.js'),
+    build: path.join(__dirname, 'public'),
+    output: path.join(__dirname, 'public', 'bundle.js'),
+    components: path.join(__dirname, 'client', 'app', 'components')
+};
 
 module.exports = {
     entry: [
         paths.entry
     ],
     output: {
-        path: __dirname,
-        filename: paths.output,
+        path: paths.build,
+        filename: 'bundle.js',
     },
     resolve: {
-        root: __dirname,
+        root: paths.components,
         alias: {
-            AliasName: 'alias-value'
+            Main: 'Main.js'
         },
         extensions: ['', '.js', '.jsx']
     },
@@ -24,8 +26,7 @@ module.exports = {
         loaders: [
             {
                 loader: 'babel-loader',
-
-                test: /\.jsx?$/,
+                test: /\.js?$/,
                 exclude: /(node_modules|bower_components)/
             },
             {
