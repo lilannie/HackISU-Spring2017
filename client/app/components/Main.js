7cbc63bf49916e8d52d979e070aad9413031a921
Annie Steenson <anniesteenso@gmail.com>
2017-02-25 22:50:50 -0600

    Sounds are now generated from the database

--- client/app/components/Main.js
+++ client/app/components/Main.js
@@ -15,83 +15,95 @@ export default class Main extends React.Component {
             rootRef: undefined,
             roomId: 0,
             data: {
-                "phrases": [
-                    {
-                        "id": 0,
-                        "name": "Quick Rifts"
-                    },
-                    {
-                        "id": 1,
-                        "name": "Slow Rifts"
-                    },
-                    {
-                        "id": 2,
-                        "name": "Fast Tempo"
-                    },
-                    {
-                        "id": 3,
-                        "name": "Slow Tempo"
-                    }],
-                "rooms": [
-                    {
-                        "activePhrase": 1,
-                        "id": 0,
-                        "name": "Annie's Room",
-                        "songId": 0,
-                        "url": "hi.com"
-                    },
-                    {
-                        "activePhrase": 2,
-                        "id": 1,
-                        "name": "Annie's 2nd Room",
-                        "songId": 1,
-                        "url": "other.com"
-                    }],
-                "songs": [
-                    {
-                        "id": 0,
-                        "name": "Rock and Roll",
-                        "phrases": [0, 1]
-                    },
-                    {
-                        "id": 1,
-                        "name": "Smooth Blues",
-                        "phrases": [null, null, 2, 3]
-                    }],
-                "sound-groups": [
-                    {
-                        "id": 0,
-                        "name": "Drums",
-                        "sounds": [0, 1]
-                    },
-                    {
-                        "id": 1,
-                        "name": "Synths",
-                        "sounds": [null, null, 2, 3]
-                    }],
-                "sounds": [
-                    {
-                        "file": "file.mp4",
-                        "id": 0,
-                        "name": "Trumpet"
-                    },
-                    {
-                        "file": "file.mp4",
-                        "id": 1,
-                        "name": "Bass"
-                    }]
+                "messages" : [ {
+                    "authorId" : 0,
+                    "id" : 0,
+                    "order" : 0,
+                    "roomId" : 0,
+                    "text" : "Hi There!",
+                    "timeSent" : "2017-02-20-10:00:00"
+                }, {
+                    "id" : 1,
+                    "order" : 0,
+                    "roomId" : 1,
+                    "text" : "Whats up"
+                } ],
+                "phrases" : [ {
+                    "id" : 0,
+                    "name" : "Quick Rifts"
+                }, {
+                    "id" : 1,
+                    "name" : "Slow Rifts"
+                }, {
+                    "id" : 2,
+                    "name" : "Fast Tempo"
+                }, {
+                    "id" : 3,
+                    "name" : "Slow Tempo"
+                } ],
+                "rooms" : [ {
+                    "activePhrase" : 1,
+                    "id" : 0,
+                    "messages" : [ 0 ],
+                    "name" : "Annie's Room",
+                    "songId" : 0,
+                    "url" : "hi.com"
+                }, {
+                    "activePhrase" : 2,
+                    "id" : 1,
+                    "name" : "Annie's 2nd Room",
+                    "songId" : 1,
+                    "url" : "other.com"
+                } ],
+                "songs" : [ {
+                    "id" : 0,
+                    "name" : "Rock and Roll",
+                    "phrases" : [ 0, 1 ]
+                }, {
+                    "id" : 1,
+                    "name" : "Smooth Blues",
+                    "phrases" : [ null, null, 2, 3 ]
+                } ],
+                "sound-groups" : [ {
+                    "id" : 0,
+                    "name" : "Drums",
+                    "sounds" : [ 0, 1 ]
+                }, {
+                    "id" : 1,
+                    "name" : "Synths",
+                    "sounds" : [ null, null, 2, 3 ]
+                } ],
+                "sounds" : [ {
+                    "file" : "file.mp4",
+                    "id" : 0,
+                    "name" : "Trumpet"
+                }, {
+                    "file" : "file.mp4",
+                    "id" : 1,
+                    "name" : "Bass"
+                } ]
             }
+
         }
     }
 
     componentDidMount() {
-        const rootRef = firebase.database().ref();
+        const root = firebase.database().ref();
 
-        rootRef.on('value', snap => {
+        root.once('value', snap => {
             this.setState({
                 data: snap.val()
             });
         });
+
+        root.on('value', snap => {
+            this.setState({
+                data: snap.val()
+            });
+        });
+
+        console.log("Initializing Data:");
+        console.log(JSON.stringify(this.state.data));
     }
 
     render() {
@@ -99,7 +111,7 @@ export default class Main extends React.Component {
             <div className="main-container">
                 <div className="row">
                     <SidebarLeft />
-                    <MainContent />
+                    <MainContent data={this.props.data} />
                     <SidebarRight />
                 </div>
                 <Footer />
--- client/app/components/Main/SoundGroup.js
+++ client/app/components/Main/SoundGroup.js
@@ -6,64 +6,59 @@ import SoundToggle from './SoundToggle';
 import '../../scss/main.scss';
 
 export default class SoundGroup extends React.Component {
-    defaultProps = {
-        groupTitle: 'Drums',
-        sounds: [
+    static defaultProps = {
+        type: 'toggle',
+        group: {
+            "id": 0,
+            "name": "Drums",
+            "sounds": [0, 1]
+        },
+        "sounds": [
             {
-                id: 0,
-                name: 'Jazz Bass'
+                "file": "file.mp4",
+                "id": 0,
+                "name": "Trumpet"
             },
             {
-                id: 1,
-                name: 'Kick Drum'
+                "file": "file.mp4",
+                "id": 1,
+                "name": "Bass"
             },
             {
+                "file": "file.mp4",
                 id: 2,
                 name: 'Synth 1'
             },
             {
+                "file": "file.mp4",
                 id: 3,
                 name: 'Synth 2'
             },
             {
+                "file": "file.mp4",
                 id: 4,
                 name: 'Tweet'
             },
             {
+                "file": "file.mp4",
                 id: 5,
                 name: 'Chirp'
+            },
+            {
+                "file": "file.mp4",
+                id: 0,
+                name: 'Jazz Bass'
+            },
+            {
+                "file": "file.mp4",
+                id: 1,
+                name: 'Kick Drum'
             },
         ]
     };
 
     state = {
-        groupTitle: 'Drums',
-        sounds: [
-            {
-                id: 0,
-                name: 'Jazz Bass'
-            },
-            {
-                id: 1,
-                name: 'Kick Drum'
-            },
-            {
-                id: 2,
-                name: 'Synth 1'
-            },
-            {
-                id: 3,
-                name: 'Synth 2'
-            },
-            {
-                id: 4,
-                name: 'Tweet'
-            },
-            {
-                id: 5,
-                name: 'Chirp'
-            },
-        ]
+        soundsOn: []
     };
 
     constructor(props) {
@@ -73,40 +68,57 @@ export default class SoundGroup extends React.Component {
     }
 
     getButtons() {
-        return this.state.sounds.map(function (sound) {
+        let sounds = this.props.sounds;
+        return this.props.group.sounds.map(function (id) {
+            let sound = sounds[id];
+
             return (
                 <SoundButton
                     key={'sound-' + sound.id}
                     soundId={sound.id}
-                    soundName={sound.name}/>
+                    soundName={sound.name}
+                    file={sound.file}
+                />
             );
         });
     }
 
     getToggles() {
-        return this.state.sounds.map(function (sound) {
-            return (
-                <SoundToggle
-                    key={'sound-' + sound.id}
-                    soundId={sound.id}
-                    soundName={sound.name}/>
-            );
+        let sounds = this.props.sounds;
+
+        return this.props.group.sounds.map(function (item) {
+            let sound = sounds[item];
+            if (sound != null && sound != undefined) {
+                return (
+                    <SoundToggle
+                        key={'sound-' + sound.id}
+                        soundId={sound.id}
+                        soundName={sound.name}
+                        file={sound.file}
+                    />
+                );
+            }
+            else {
+                return;
+            }
+
         });
     }
 
     render() {
+        var buttons = (this.getToggles());
+        if (this.props.type === 'buttons') {
+            buttons = (this.getButtons());
+        }
         return (
             <div className="sound-group-container row">
                 <div className="sound-group container-fluid">
                     <h3 className="t-h3">
-                        {this.state.groupTitle}
+                        {this.props.group.name}
                     </h3>
                     <div className="row">
                         <div className="sound-content">
-
-                                {this.getToggles()}
-                                {this.getButtons()}
-
+                            {buttons}
                         </div>
                     </div>
                 </div>
