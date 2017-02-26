4883f5c590442defa45c15c35c2951cf12c1a90a
Annie Steenson <anniesteenso@gmail.com>
2017-02-25 21:45:27 -0600

    Added dummy props

--- client/app/components/Main.js
+++ client/app/components/Main.js
@@ -12,18 +12,86 @@ export default class Main extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            rootRef: undefined
+            rootRef: undefined,
+            roomId: 0,
+            data: {
+                "phrases": [
+                    {
+                        "id": 0,
+                        "name": "Quick Rifts"
+                    },
+                    {
+                        "id": 1,
+                        "name": "Slow Rifts"
+                    },
+                    {
+                        "id": 2,
+                        "name": "Fast Tempo"
+                    },
+                    {
+                        "id": 3,
+                        "name": "Slow Tempo"
+                    }],
+                "rooms": [
+                    {
+                        "activePhrase": 1,
+                        "id": 0,
+                        "name": "Annie's Room",
+                        "songId": 0,
+                        "url": "hi.com"
+                    },
+                    {
+                        "activePhrase": 2,
+                        "id": 1,
+                        "name": "Annie's 2nd Room",
+                        "songId": 1,
+                        "url": "other.com"
+                    }],
+                "songs": [
+                    {
+                        "id": 0,
+                        "name": "Rock and Roll",
+                        "phrases": [0, 1]
+                    },
+                    {
+                        "id": 1,
+                        "name": "Smooth Blues",
+                        "phrases": [null, null, 2, 3]
+                    }],
+                "sound-groups": [
+                    {
+                        "id": 0,
+                        "name": "Drums",
+                        "sounds": [0, 1]
+                    },
+                    {
+                        "id": 1,
+                        "name": "Synths",
+                        "sounds": [null, null, 2, 3]
+                    }],
+                "sounds": [
+                    {
+                        "file": "file.mp4",
+                        "id": 0,
+                        "name": "Trumpet"
+                    },
+                    {
+                        "file": "file.mp4",
+                        "id": 1,
+                        "name": "Bass"
+                    }]
+            }
         }
     }
 
     componentDidMount() {
         const rootRef = firebase.database().ref();
-        const rooms = rootRef.child('rooms');
-        // number.on('value', snap => {
-        //     this.setState({
-        //         number: snap.val()
-        //     });
-        // });
+
+        rootRef.on('value', snap => {
+            this.setState({
+                data: snap.val()
+            });
+        });
     }
 
     render() {
